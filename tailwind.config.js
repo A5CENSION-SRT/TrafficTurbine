299ca15e79475ab78153f841a199031f2c73aa73
snehal <snehalreddy@gmail.com>
2025-06-23 16:46:23 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4fd8', // Change this value
+        desktop: '#6366f1',
+        mobile: '#f59e42',
+        // ...other custom colors...
+      },
+    },
+  },
+  // ...existing code...
+}
